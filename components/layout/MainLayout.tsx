5757e2d36b7ffb0317c31e7d6d699eb72ce2dfc8
mejomba <moojaminzadeh@gmail.com>
2025-08-08 20:33:38 +0330

    add comment for MainLayout

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -7,17 +7,27 @@ import { Box } from '@mui/material';
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <Box display="flex" flexDirection="column" minHeight="100vh">
+            {/* begin header section */}
             <Header />
+            {/* end header section */}
 
+            {/* begin page content */}
             <Box display="flex" flex="1">
-                <Box component="main" flex="1" p={3}>
-                    {children}
-                </Box>
 
+                {/* begin right sidebar */}
                 <Box width="240px">
                     <Sidebar />
                 </Box>
+                {/* end right sidebar */}
+
+                {/* begin main content */}
+                <Box component="main" flex="1" p={3}>
+                    {children}
+                </Box>
+                {/* end main content */}
+
             </Box>
+            {/* end page content */}
 
             <Footer />
         </Box>
