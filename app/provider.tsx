d4bcf3c7cb7e2bc272014703c80f9a4b28a80490
mejomba <moojaminzadeh@gmail.com>
2025-09-18 21:48:24 +0330

    add provider for loading

---
+++ app/provider.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useEffect } from "react"
+import { usePathname } from "next/navigation"
+import NProgress from "nprogress"
+
+export default function ProgressBarProvider() {
+    const pathname = usePathname()
+
+    useEffect(() => {
+        NProgress.start()
+
+        const timeout = setTimeout(() => {
+            NProgress.done()
+        }, 300)
+
+        return () => {
+            clearTimeout(timeout)
+            NProgress.done()
+        }
+    }, [pathname])
+
+    return null
+}
